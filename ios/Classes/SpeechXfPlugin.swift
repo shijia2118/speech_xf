8cc9c4ce51032e072a626f839e02b5ef479c3dba
潇洒的然然 <shijia2118@126.com>
2023-05-04 22:46:55 +0800

    no message

--- ios/Classes/SpeechXfPlugin.swift
+++ ios/Classes/SpeechXfPlugin.swift
@@ -1,6 +1,7 @@
 import Flutter
 import UIKit
 
+
 public class SpeechXfPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "speech_xf", binaryMessenger: registrar.messenger())
